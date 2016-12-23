4ce6ec2f610a7f607c602b6c6f05db9e550356fe
gl1293 <gl1293@nyu.edu>
2016-12-23 01:06:25 -0500

    Final Project

---
+++ extras.js
@@ -0,0 +1,21 @@
+//////////////////////////////
+
+
+/*
+///////////////////////////////
+// function player() {
+//  input = createInput();
+//  input.changed(name)
+//}
+
+
+
+    if (currentSceneNumber == 4) {
+    text(currentScene.spirit[1], x, y),
+      text(currentScene.scenario[1], x, y + 20);
+    if (frameCount % 50 === 0) {
+      specialCase();
+    }
+  } 
+  put this part at the top of the draw function and change if statements to else if
+  */
